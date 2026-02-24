519ee822d3b6ec9f4ce94081176a23371ce3b40d
sandradevia <144318268+sandradevia@users.noreply.github.com>
2026-02-24 11:28:49 +0700

    Delete service directory

--- service/categoryService.ts
+++
@@ -1,7 +0,0 @@
-import axios from "../lib/axios";
-import { Category } from "types/category";
-
-export const getCategories = async () => {
-  const response = await axios.get("/category");
-  return response.data;
-};
